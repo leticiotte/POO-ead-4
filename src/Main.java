c9eaa27fdf108c32ba4a1e0fc03d83b7767a6a3f
Letícia Gonçalves <leticiaespregagon@gmail.com>
2022-05-29 19:34:49 -0300

    fix: ajusta criação do banco de dados

--- src/Main.java
+++ src/Main.java
@@ -13,22 +13,22 @@ public class Main {
         PartidoSQLiteDAO partidoDAO = new PartidoSQLiteDAO();
         CandidatoSQLiteDAO candidatoDAO = new CandidatoSQLiteDAO();
 
-//        Cargo cargo1 = new Cargo(1,"Presidente");
-//        Cargo cargo2 = new Cargo(2,"Vereador");
-//        cargoDAO.salvar(cargo1);
-//        cargoDAO.salvar(cargo2);
-//
-//        Partido partido1 = new Partido(13, "Partido dos Trabalhadores", "PT");
-//        Partido partido2 = new Partido(50, "Partido Socialismo e Liberdade", "PSOL");
-//        partidoDAO.salvar(partido1);
-//        partidoDAO.salvar(partido2);
-////
-//        Candidato candidato1 = new Candidato(13,"Lula", cargo1, partido1);
-//        Candidato candidato2 = new Candidato(50501,"Djalma Nery", cargo2, partido2);
-//        candidatoDAO.salvar(candidato1);
-//        candidatoDAO.salvar(candidato2);
-//
-//        candidatoDAO.apagar(50501);
+        Cargo cargo1 = new Cargo(1,"Presidente");
+        Cargo cargo2 = new Cargo(2,"Vereador");
+        cargoDAO.salvar(cargo1);
+        cargoDAO.salvar(cargo2);
+
+        Partido partido1 = new Partido(13, "Partido dos Trabalhadores", "PT");
+        Partido partido2 = new Partido(50, "Partido Socialismo e Liberdade", "PSOL");
+        partidoDAO.salvar(partido1);
+        partidoDAO.salvar(partido2);
+
+        Candidato candidato1 = new Candidato(13,"Lula", cargo1, partido1);
+        Candidato candidato2 = new Candidato(50501,"Djalma Nery", cargo2, partido2);
+        candidatoDAO.salvar(candidato1);
+        candidatoDAO.salvar(candidato2);
+
+        candidatoDAO.apagar(50501);
 
         List<Cargo> listaCargos = cargoDAO.buscarTodos();
         for (Cargo c : listaCargos )
